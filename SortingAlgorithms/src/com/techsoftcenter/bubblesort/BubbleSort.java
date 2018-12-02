57a1af3d7ac321386963e8c0a0c3d9f5e3b5d088
cihan biber <cihan.forrestgump@gmail.com>
2018-12-02 19:28:46 +0300

    sorting - BubbleSort Implementation

---
+++ SortingAlgorithms/src/com/techsoftcenter/bubblesort/BubbleSort.java
@@ -0,0 +1,123 @@
+package com.techsoftcenter.bubblesort;
+
+import java.util.concurrent.ThreadLocalRandom;
+
+/**
+ * Worst and Average Case Time Complexity: O(n*n). Worst case occurs when array is reverse sorted.
+ * Best Case Time Complexity: O(n). Best case occurs when array is already sorted.
+ * Auxiliary Space: O(1)
+ * Boundary Cases: Bubble sort takes minimum time (Order of n) when elements are already sorted.
+ * Sorting In Place: Yes
+ * Stable: Yes
+ */
+public class BubbleSort {
+
+    public static void main(String[] args) {
+        int[] array = new int[10];
+
+        fillArrayRandom(array, -50, 50);
+
+        printArray(array);
+
+        System.out.println("Bubble sort started...");
+        long startTime = System.nanoTime();
+        sort(array);
+        long endTime = System.nanoTime();
+        long time_ns = endTime - startTime;
+        System.out.println("Bubble sort finished " + time_ns + " ns");
+        System.out.println("--------------------------------------------");
+
+        printArray(array);
+
+        System.out.println("Improved Bubble sort started...");
+        startTime = System.nanoTime();
+        optimizedSort(array);
+        endTime = System.nanoTime();
+        time_ns = endTime - startTime;
+        System.out.println("Improved Bubble sort finished " + time_ns + " ns");
+
+        printArray(array);
+
+        System.out.println("Recursive Bubble sort started...");
+        startTime = System.nanoTime();
+        recursiveSort(array, array.length);
+        endTime = System.nanoTime();
+        time_ns = endTime - startTime;
+        System.out.println("Recursive Bubble sort finished " + time_ns + " ns");
+
+        printArray(array);
+    }
+
+    private static void sort(int[] array) {
+        for(int i=0; i<array.length-1; i++){
+            for (int j=0; j<array.length - i - 1; j++){
+//                printArray(array);
+                if (array[j]>array[j+1]) {
+                    swap(array, j, j+1);
+                }
+            }
+        }
+    }
+
+    private static void recursiveSort(int array[], int length){
+        if (length == 1){
+            return;
+        }
+
+        for(int i=0; i<length-1; i++){
+            if (array[i]>array[i+1]) {
+                swap(array, i, i+1);
+            }
+        }
+        recursiveSort(array, length -1);
+    }
+
+    private static void optimizedSort(int arr[])
+    {
+        int i, j, temp;
+        boolean swapped;
+        int n = arr.length;
+        for (i = 0; i < n - 1; i++)
+        {
+            swapped = false;
+            for (j = 0; j < n - i - 1; j++)
+            {
+                if (arr[j] > arr[j + 1])
+                {
+                    swap(arr, j + 1, j);
+                    swapped = true;
+                }
+            }
+
+            // IF no two elements were
+            // swapped by inner loop, then break
+            if (!swapped)
+                break;
+        }
+    }
+
+    private static void swap(int[] array, int from, int to) {
+        if (array[from] != array[to]) {
+            int temp = array[to];
+            array[to] = array[from];
+            array[from] = temp;
+        }
+    }
+
+    private static void fillArrayRandom(int[] arr, int min, int max){
+        for (int i=0; i<arr.length; i++){
+            int randomNum = ThreadLocalRandom.current().nextInt(min, max + 1);
+            arr[i] = randomNum;
+        }
+    }
+
+    private static void printArray(int[] array){
+        System.out.print("[ ");
+        for (int item: array) {
+            System.out.print(item + "  ");
+        }
+        System.out.println("]");
+    }
+
+
+}
