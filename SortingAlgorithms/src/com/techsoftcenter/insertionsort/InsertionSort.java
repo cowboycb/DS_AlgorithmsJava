f45a9bd4296ca686a94f8cee81dbcbde53e371ad
cihan biber <bibercihan@gmail.com>
2018-12-10 23:36:40 +0300

    sorting - Insertion sort implementation

---
+++ SortingAlgorithms/src/com/techsoftcenter/insertionsort/InsertionSort.java
@@ -0,0 +1,39 @@
+package com.techsoftcenter.insertionsort;
+
+import com.techsoftcenter.util.SortUtil;
+
+public class InsertionSort {
+
+    public static void main(String[] args) {
+        int[] array = new int[10];
+        SortUtil.fillArrayRandom(array, -50, 50);
+
+        SortUtil.printArray(array);
+
+        System.out.println("Insertion sort started...");
+        long startTime = System.nanoTime();
+        sort(array);
+        long endTime = System.nanoTime();
+        long time_ns = endTime - startTime;
+        System.out.println("Insertion sort finished " + time_ns + " ns");
+        System.out.println("--------------------------------------------");
+        SortUtil.printArray(array);
+
+    }
+
+    private static void sort(int[] array) {
+        for (int i=1; i<array.length; i++){
+            int key = array[i];
+            int j=i;
+            while(j>0 && key<array[j-1]){
+                SortUtil.swap(array, j, j-1);
+                j--;
+            }
+            if (i != j) {
+                array[j] = key;
+            }
+        }
+
+    }
+
+}
